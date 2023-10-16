e33984a209aaf6b1dbf85769cbccacb6e77eb618
dohodin <WrapPoulet2022>
2023-10-16 14:39:41 +0200

    Removing useless import

--- Source/server/index.js
+++ Source/server/index.js
@@ -1,7 +1,6 @@
 // importing modules
 const express = require("express");
 const http = require("http");
-const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
