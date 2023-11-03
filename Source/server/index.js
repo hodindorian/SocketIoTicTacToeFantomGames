46e4b4b69d069190a5c9859a8ef1680cdf4b1527
dorian.hodin <dorian.hodin@etu.uca.fr>
2023-11-03 11:40:46 +0100

    Change to HTTPS

--- Source/server/index.js
+++ Source/server/index.js
@@ -1,10 +1,10 @@
 // importing modules
 const express = require("express");
-const http = require("http");
+const https = require("https");
 
 const app = express();
 const port = process.env.PORT || 3000;
-var server = http.createServer(app);
+var server = https.createServer(app);
 const Room = require("./models/room");
 const io = require("socket.io")(server, {
     cors: {
